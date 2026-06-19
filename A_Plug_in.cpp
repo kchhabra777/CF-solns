ddda22a3b271d5bd1dd333d603fb3e2f3cced17e
Krishna <krishcarbon7@gmail.com>
2026-06-19 18:15:42 +0530

    initial commit

---
+++ A_Plug_in.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+
+void solve() {
+    // Your code
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
